63c84f6f0b0bb2a8e2b124008015942cc51f1234
Patrik <patriknilsson1992@gmail.com>
2019-06-23 18:03:58 +0700

    removing boilerpalte code, create empty search view component

---
+++ src/views/Search.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="search">
+    <h2>search</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Search"
+};
+</script>
+
+<script>
+export default {
+  name: "Search"
+};
+</script>
