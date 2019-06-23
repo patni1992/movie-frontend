740ada8c304ff72b0e14271cc7f1c6b9cb9fdc40
Patrik <patriknilsson1992@gmail.com>
2019-06-23 22:05:31 +0700

    Fetch tv-shows, loop through, create MovieCard

---
+++ src/components/MovieCard.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="movie-card">
+    <img class="movie-card__img" :src="movie.image" alt="" />
+    <p class="movie-card__name">{{ movie.name }}</p>
+    <div class="movie-card__categories">
+      <span v-for="category in movie.categories" :key="category">
+        {{ category }}
+      </span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MovieCard",
+  props: {
+    movie: Object
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.movie-card__img {
+  width: 100%;
+}
+.movie-card__name {
+  margin-bottom: 0.5rem;
+}
+.movie-card__categories {
+  font-size: 0.6rem;
+  span:not(:first-child) {
+    margin-left: 0.3rem;
+  }
+}
+</style>
--- src/views/Search.vue
+++ src/views/Search.vue
@@ -1,17 +1,50 @@
 <template>
   <div class="search">
     <h2>search</h2>
+    <div class="wrapper">
+      <div class="grid-container">
+        <movie-card
+          class="grid-item"
+          v-for="movie in movies"
+          :key="movie.id"
+          :movie="movie"
+        />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import MovieCard from "@/components/MovieCard";
 export default {
-  name: "Search"
+  components: {
+    MovieCard
+  },
+  data() {
+    return {
+      movies: []
+    };
+  },
+  name: "Search",
+  mounted() {
+    this.$api.get("/tv-shows").then(resp => {
+      console.log(resp.data.data);
+      this.movies = resp.data.data;
+    });
+  }
 };
 </script>
 
-<script>
-export default {
-  name: "Search"
-};
-</script>
+<style scoped>
+.grid-container {
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+}
+.grid-item {
+  padding: 0 1.2rem 1.2rem;
+  max-width: 9rem;
+}
+</style>
