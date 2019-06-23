7597df029eebd76c4201de926129cc5b10461001
Patrik <patriknilsson1992@gmail.com>
2019-06-24 05:19:26 +0700

    impelment MovieCard

--- src/components/MovieCard.vue
+++ src/components/MovieCard.vue
@@ -1,8 +1,9 @@
 <template>
-  <div class="movie-card">
-    <img class="movie-card__img" :src="movie.image" alt="" />
-    <p class="movie-card__name">{{ movie.name }}</p>
-    <div class="movie-card__categories">
+  <div class="movie">
+    <div class="movie__rating movie__rating--great">{{ movie.rating }}</div>
+    <img class="movie__img" :src="movie.image" alt="" />
+    <p class="movie__name">{{ movie.name }}</p>
+    <div class="movie__categories">
       <span v-for="category in movie.categories" :key="category">
         {{ category }}
       </span>
@@ -20,16 +21,52 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.movie-card__img {
+.movie {
+  position: relative;
+}
+.movie__img {
   width: 100%;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
 }
-.movie-card__name {
-  margin-bottom: 0.5rem;
+.movie__name {
+  margin-top: 5px;
+  margin-bottom: 0.2rem;
+  font-size: 0.8rem;
+  font-weight: bold;
 }
-.movie-card__categories {
-  font-size: 0.6rem;
+.movie__categories {
+  color: #adb3ba;
+  margin-top: 0;
+  font-size: 9px;
   span:not(:first-child) {
-    margin-left: 0.3rem;
+    margin-left: 0.1rem;
+  }
+}
+
+.movie__rating {
+  color: white;
+  position: absolute;
+  top: -0.5rem;
+  right: 0.8rem;
+  text-align: center;
+  border-radius: 50%;
+  width: 1.5rem;
+  height: 1.5rem;
+  display: flex;
+  font-size: 0.6rem;
+  justify-content: center;
+  align-items: center;
+
+  &--great {
+    background: #41a375;
+  }
+
+  &--good {
+    background: #e1be57;
+  }
+
+  &--bad {
+    background: #c70d0b;
   }
 }
 </style>
