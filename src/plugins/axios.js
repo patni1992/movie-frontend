193c71bb9bd5cdb720557ce47b0ed3f70d5e3cde
Patrik <patriknilsson1992@gmail.com>
2019-06-23 21:46:52 +0700

    fix small misstake

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export default {
   install(Vue) {
     Vue.prototype.$api = axios.create({
-      baseURL: process.env.VUE_APP_APP_URL
+      baseURL: process.env.VUE_APP_API_URL
     });
   }
 };
