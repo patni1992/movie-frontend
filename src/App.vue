30d3bd4153420525f20779942c82e175c5855d85
Patrik <patriknilsson1992@gmail.com>
2019-06-24 05:18:26 +0700

    set some default styling, add css variables

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,17 @@
 </template>
 
 <style>
-@import url('https://fonts.googleapis.com/css?family=Lato:400,700&display=swap');
-body, html {
+@import url("https://fonts.googleapis.com/css?family=Lato:400,700&display=swap");
+body,
+html {
+  --theme-color: #c70d0b;
+  --main-gray: #626263;
+  --sidebar-width: 12.5rem;
+  width: 100%;
+  height: 100%;
   font-family: "Lato", sans-serif;
+  color: var(--main-gray);
+  margin: 0;
+  padding: 0;
 }
 </style>
